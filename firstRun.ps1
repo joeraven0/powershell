11687cf3fc8b4163f5d97258128d23d4f802c3db
Joe Raven <joakim.ringstad.raavi@gmail.com>
2022-08-27 02:44:53 +0200

    Update firstRun.ps1

--- firstRun.ps1
+++ firstRun.ps1
@@ -25,6 +25,7 @@ function setup_settings{
     Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Feeds" -Name EnableFeeds -Value 0 -Type DWORD -Force
     Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name StoreAppsOnTaskbar -Value 0 -Type DWord -Force
     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableFirstLogonAnimation -Value 0 -Type DWord -Force
+    Get-ChildItem -Path "HKCU:\AppEvents\Schemes\Apps" | Get-ChildItem | Get-ChildItem | Where-Object {$_.PSChildName -eq ".Current"} | Set-ItemProperty -Name "(Default)" -Value ""
 }
 function uninstall_applications{
     #$application = Get-WmiObject -Class Win32_Product -Filter "Name = 'Windows PC Health Check'"
